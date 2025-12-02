692af6ed2e21dba0c6ec7043729531a837454d25
KelliPamela <kellipamelacardenasaquino@gmail.com>
2025-12-02 12:44:04 -0600

    Add files via upload
    e

---
+++ E2V117102025.py
@@ -0,0 +1,11 @@
+#Esta es nuestra primera ventana
+
+import tkinter as tk
+
+#creacion de ventana 1
+ventana = tk.Tk()
+ventana.title("equipo 3cp test de adicciones a los videojuegos")
+ventana.geometry("600x400")
+ventana.config(bg="#FFBAE5")
+
+ventana.mainloop()
